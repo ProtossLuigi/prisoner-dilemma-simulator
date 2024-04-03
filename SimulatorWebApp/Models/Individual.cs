4317ea030380d399c0e8280f85c59800daa966d2
ProtossLuigi <protoss0@wp.pl>
2024-04-03 16:38:02 +0200

    added stuff

---
+++ SimulatorWebApp/Models/Individual.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+
+namespace SimulatorWebApp.Models
+{
+    public class Individual
+    {
+        public readonly int memoryLength;
+        public readonly double moodThreshold;
+        public readonly double[,,] weights;
+
+        public Individual(int memoryLength, double moodThreshold, double[,,] weights)
+        {
+            this.memoryLength = memoryLength;
+            this.moodThreshold = moodThreshold;
+            this.weights = weights;
+        }
+
+        public bool Act(List<(bool, bool)> gameState)
+        {
+            var len = gameState.Count < memoryLength ? gameState.Count : memoryLength;
+            var currMood = 0d;
+            for ( var i = 0; i < len; i++ )
+            {
+                for ( var player = 0; player < len; player++ )
+                {
+                    currMood += weights[i, player, (player == 0 ? gameState[i].Item1 : gameState[i].Item2) ? 1 : 0];
+                }
+            }
+            return currMood > moodThreshold;
+        }
+    }
+}
---
+++ SimulatorWebApp/Services/GameService.cs
@@ -0,0 +1,58 @@
+﻿using SimulatorWebApp.Models;
+
+namespace SimulatorWebApp.Services
+{
+    public class GameService
+    {
+        private int roundLength = 500;
+
+        private int coopVal = 3;
+        private int defectWinVal = 5;
+        private int defectLossVal = 0;
+        private int doubleDefectVal = 1;
+
+        public GameService()
+        {
+
+        }
+
+        public (int, int) PlayMatch(Individual player1, Individual player2)
+        {
+            var gameState = new List<(bool, bool)>();
+            int player1Score = 0;
+            int player2Score = 0;
+            for (int i = 0; i < roundLength; i++)
+            {
+                gameState.Insert(0, (player1.Act(gameState), player2.Act(gameState)));
+
+                if (gameState[0].Item1)
+                {
+                    if (gameState[0].Item2)
+                    {
+                        player1Score += coopVal;
+                        player2Score += coopVal;
+                    }
+                    else
+                    {
+                        player1Score += defectLossVal;
+                        player2Score += defectWinVal;
+                    }
+                }
+                else
+                {
+                    if (gameState[0].Item2)
+                    {
+                        player1Score += defectWinVal;
+                        player2Score += defectLossVal;
+                    }
+                    else
+                    {
+                        player1Score += doubleDefectVal;
+                        player2Score += doubleDefectVal;
+                    }
+                }
+            }
+            return (player1Score, player2Score);
+        }
+    }
+}
